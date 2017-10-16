3d07e3c29a52527a8bed6186bb6f966cd34cdfca
sihadan <def6488@gmail.com>
2017-10-17 01:16:48 +0900

    add scripts

---
+++ scripts/cms_user_adder.py
@@ -0,0 +1,78 @@
+from subprocess import call
+import sys
+import random
+import hashlib
+
+
+def parse_info(info):
+
+    # this function must return an array of dictionary.
+    # each element should have username, password, first_name, last_name
+
+    '''
+
+    # PARSING SAMPLE
+    # YOU SHOULD IMPLEMNET THIS BY USERSELF
+
+    data = []
+    cnt = 1
+    for row in info:
+        r = row.strip().split()
+        pw = hashlib.md5(str(random.random()).encode()).hexdigest()
+
+        d = {
+            'username': r[0],
+            'password': pw,
+            'first_name': r[1],
+            'last_name': r[2],
+        }
+
+        data.append(d)
+        cnt += 1
+
+    return data
+
+    '''
+
+    return [
+        {
+            'username': 'iam',
+            'password': 'mr',
+            'first_name': 'Donald',
+            'last_name': 'Trump',
+        }
+    ]
+
+
+def main():
+
+    if len(sys.argv) < 2:
+        print("[*] Usage : python3 %s <user info file>" % sys.argv[0])
+        exit()
+
+    user_info_file = sys.argv[1]
+
+    users = open(user_info_file, 'r').readlines()
+    user_info = parse_info(users)
+
+    login_info = ['Username,Password']
+
+    for info in user_info:
+
+        # $ cmsAddUser <first name> <last name> <username> -p <password>
+        call(['cmsAddUser', info['first_name'], info['last_name'], info['username'],
+              '-p', info['password']])
+
+        # user login info
+        login_info.append('%s,%s' % (info['username'], info['password']))
+
+    # saves user login info in csv format
+    login_info_file = 'login_info.csv'
+    with open(login_info_file, 'w') as f:
+        for info in login_info:
+            f.write(info + '\n')
+
+    print('[+] Done Adding user, file saved')
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/cms_user_participater.py
@@ -0,0 +1,69 @@
+from subprocess import call
+import sys
+import random
+import hashlib
+
+
+def parse_info(info):
+
+    # this function must return an array of dictionary.
+    # each element should have username which is added as a user
+    # you may use same function used in 'cms_user_adder'
+
+    '''
+
+    # PARSING SAMPLE
+    # YOU SHOULD IMPLEMNET THIS BY USERSELF
+
+    data = []
+    cnt = 1
+    for row in info:
+        r = row.strip().split()
+        pw = hashlib.md5(str(random.random()).encode()).hexdigest()
+
+        d = {
+            'username': r[0],
+            'password': pw,
+            'first_name': r[1],
+            'last_name': r[2],
+        }
+
+        data.append(d)
+        cnt += 1
+
+    return data
+
+    '''
+
+    return [
+        {
+            'username': 'iam',
+            'password': 'mr',
+            'first_name': 'Donald',
+            'last_name': 'Trump',
+        }
+    ]
+
+
+def main():
+
+    if len(sys.argv) < 3:
+        print("[*] Usage : python3 %s <user info file> <contest_id>" % sys.argv[0])
+        exit()
+
+    user_info_file = sys.argv[1]
+    contest_id = sys.argv[2]
+
+    users = open(user_info_file, 'r').readlines()
+    user_info = parse_info(users)
+
+    for info in user_info:
+
+        # $ cmsAddParticipation <username> -c <contest_id>
+        call(['cmsAddParticipation', info['username'],
+              '-c', contest_id])
+
+    print('[+] Done adding participation')
+
+if __name__ == '__main__':
+    main()
